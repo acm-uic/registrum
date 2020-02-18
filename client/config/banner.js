8d660e1534b7ab71e5e4b85321cfbb646fe1ba53
Alex Chomiak <alex.chomiak@gmail.com>
2020-02-18 13:21:30 -0600

    :bug: fixed print width lol

--- client/config/banner.js
+++ client/config/banner.js
@@ -4,7 +4,7 @@
  * Author: Alex Chomiak
  *
  * Last Modified: Wednesday, 11th December 2019 11:16:05 pm
- * Modified By: Alex Chomiak 
+ * Modified By: Alex Chomiak
  *
  * Author Github: https://github.com/alexchomiak
  */
@@ -13,48 +13,10 @@ const webpack = require('webpack')
 const os = require('os')
 module.exports = new webpack.BannerPlugin(
     [
-        '----------------------------------------------------------------------------------------------',
-
-        '          GGGGGGGGGGGGG                          tttt',
-        '       GGG:::::::::::: G                       ttt::: t',
-        '     GG::::::::::::::: G                       t::::: t',
-        '    G::::: GGGGGGGG:::: G                       t::::: t',
-        '   G::::: G       GGGGGG   ooooooooooo   ttttttt::::: ttttttt       ooooooooooo',
-        '  G::::: G               oo::::::::::: oo t::::::::::::::::: t     oo::::::::::: oo',
-        '  G::::: G              o::::::::::::::: ot::::::::::::::::: t    o::::::::::::::: o',
-        '  G::::: G    GGGGGGGGGGo::::: ooooo::::: otttttt::::::: tttttt    o::::: ooooo::::: o',
-        '  G::::: G    G:::::::: Go:::: o     o:::: o      t::::: t          o:::: o     o:::: o',
-        '  G::::: G    GGGGG:::: Go:::: o     o:::: o      t::::: t          o:::: o     o:::: o',
-        '  G::::: G        G:::: Go:::: o     o:::: o      t::::: t          o:::: o     o:::: o',
-        '   G::::: G       G:::: Go:::: o     o:::: o      t::::: t    tttttto:::: o     o:::: o',
-        '    G::::: GGGGGGGG:::: Go::::: ooooo::::: o      t:::::: tttt::::: to::::: ooooo::::: o',
-        '     GG::::::::::::::: Go::::::::::::::: o      tt:::::::::::::: to::::::::::::::: o',
-        '       GGG:::::: GGG::: G oo::::::::::: oo         tt::::::::::: tt oo::::::::::: oo',
-        '          GGGGGG   GGGG   ooooooooooo             ttttttttttt     ooooooooooo   ',
-        '----------------------------------------------------------------------------------------------',
-        '',
-        '                          GoTo React App Bundle File',
-        `            Compiled on ${new Date().toLocaleString()}, on a ${os.type()} machine`,
-        '',
-        '**********************************************************************************************',
-        `               React Application Built Using GoTo React App (c)${new Date().getFullYear()}`,
-        '       GoTo React App is a boiler plate React Utility built by Alex Chomiak.',
-        '       It beginner friendly and more robust than create-react-app,',
-        '       the repo can be found at https://github.com/alexchomiak/goto-react-app.',
-        '       A star on the repo would be appreciated! :)',
-        '',
-        '               A CLI is available for you to get started using it right away via npm!',
-        '',
-        '                   Just run the command:',
-        '                   ****  npm install -g goto-react-app',
-        '',
-        '                   And to get started with a boiler plate project run:',
-        '                   ****  goto-react-app <project-name>',
-        '',
-        '                   Happy coding! :)',
-        '                   *** ( Full Guide is on the Repo ) ***',
-        '',
-        'Also for the wandering eye, follow me on github @ https://github.com/alexchomiak/',
-        '**********************************************************************************************'
+        '---------------------------------------------',
+        '                    Dream',
+        '                    Team',
+        '                 Final Project',
+        '---------------------------------------------'
     ].join('\n')
 )
