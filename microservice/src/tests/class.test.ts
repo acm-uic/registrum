81f205e27c6d425118ade2ff1117d401d71a8158
Alex Chomiak <alex.chomiak@gmail.com>
2020-02-22 16:40:24 -0600

    :sparkles: added some test cases

--- microservice/src/tests/class.test.ts
+++ microservice/src/tests/class.test.ts
@@ -1,5 +1,8 @@
 describe("Class Tests", () => {
     describe("Sanity Tests", () => {
         it("Correctly add class to user watch list", () => {})
+        it("Correctly remove class from user watch list", () => {})
+        it("Correctly retrieves list of class subjects from Banner DB", () => {})
+        it("Correctly retrieves classes for given subject", () => {})
     })
 })
