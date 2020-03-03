dea66c3b22437a4ee70bf1a29b3180576f2b66de
anarmawala <arshadn00@gmail.com>
2020-03-03 14:10:01 -0600

    Full Removal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,21 +12,11 @@
 import React, { FC } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Home } from '@pages/Home'
-import Header from '@components/Header'
-
-import { useSelector } from 'react-redux'
-import ManageClases from '@pages/ManageClasses'
 
 export const App: FC = () => {
-    const classes = useSelector(state => state)
-
     return (
-        <>
-            <Header />
-            <Router>
-                <Route exact path="/" component={Home} />
-            </Router>
-            <ManageClases />
-        </>
+        <Router>
+            <Route exact path="/" component={Home} />
+        </Router>
     )
 }
