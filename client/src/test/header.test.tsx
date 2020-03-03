34a04b26c1e2cec9fab524ea9ea77538adc1aa3a
anarmawala <arshadn00@gmail.com>
2020-03-03 15:51:47 -0600

    :fire: Removing old test file

--- client/src/test/header.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import { Header } from '../components/Header'
-
-it('renders', () => {
-    const result = shallow(<Header />).contains('Registrum')
-
-    expect(result).toBeTruthy()
-})
