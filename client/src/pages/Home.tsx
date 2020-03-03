c7730aacbf33855f1e9c6152fa86ca6a424403ab
anarmawala <arshadn00@gmail.com>
2020-03-03 15:42:00 -0600

    Added Pages

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -11,6 +11,8 @@
 
 import React, { FC } from 'react'
 
-export const Home: FC = () => {
-    return <div>Header</div>
+const Home: FC = () => {
+    return <div>Home</div>
 }
+
+export default Home
