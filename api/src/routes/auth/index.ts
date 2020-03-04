8f310bd597d818ea747b2fe980fc3d1a2348e2f9
Clark Chen <9372896+clarkchentw@users.noreply.github.com>
2020-03-04 17:24:00 -0600

    Add ability to return userData during SignUp and Login request (#16)
    * Result user data after account creation, but strip out the hashed password
    
    * Add userData result for login endpoint
    
    * Add comments
    
    * Add comment for all
    
    * Fix logout, was always return error, due to login structure changed.
    
    * :ambulance: Fixed BASE_URL Linked
    
    * :art: Fixed formatting
    
    * Add logout call to actually destroy login record
    
    Co-authored-by: Arshad Narmawala <arshadn00@gmail.com>

--- api/src/routes/auth/index.ts
+++ api/src/routes/auth/index.ts
@@ -1,47 +1,54 @@
-import { Router, Request, Response } from "express"
-import passport from "passport"
-import User from "../models/User"
-import bcrypt from "bcrypt"
+import { Router, Request, Response } from 'express'
+import passport from 'passport'
+import User from '../models/User'
+import bcrypt from 'bcrypt'
 // * Bind Passport stategies
-import "./passport"
-import { isAuthenticated } from "./passport"
+import './passport'
+import { isAuthenticated } from './passport'
 
 const router = Router()
 
+/* Remove data shouldn't be sent to client E.g. password */
+const stripData = userData => {
+    var result = JSON.parse(JSON.stringify(userData))
+    delete result['password']
+    return result
+}
+
 // * All routes under /auth/*
-router.get("/", isAuthenticated, (req: Request, res: Response) => {
-    res.status(200).send("OK")
+router.get('/', isAuthenticated, (req: Request, res: Response) => {
+    res.status(200).send('OK')
 })
 
+/* Login by passport.authenticate */
 router.post(
-    "/login",
-    passport.authenticate("local", { failureFlash: true }),
-    (req: Request, res: Response) => res.status(200).send("OK")
+    '/login',
+    passport.authenticate('local', { failureFlash: true }),
+    async (req: Request, res: Response) =>
+        res.status(200).json(stripData(await User.findOne({ email: req.body.email })))
 )
 
-router.post("/loginGoogle", (req: Request, res: Response) => {
+/* Login by Google - WIP to allow Student to signin with their school account*/
+router.post('/loginGoogle', (req: Request, res: Response) => {
     // TODO: Login with passport Google strategy ?
-    res.status(501).send("TODO")
+    res.status(501).send('TODO')
 })
 
-router.get("/logout", isAuthenticated, (req: Request, res: Response) => {
-    // * If session exists, destroy session, otherwise send error
-    if (req.session.user)
-        req.session.destroy(() => {
-            res.status(200).end()
-        })
-    else res.status(401).send({ error: "You cannot logout if you aren't logged in!" })
+/* Logout Route: Validation handled by passport */
+router.get('/logout', isAuthenticated, (req: Request, res: Response) => {
+    req.logout()
+    res.send('OK')
 })
 
 // * Registration route
-router.post("/signup", async (req: Request, res: Response) => {
+router.post('/signup', async (req: Request, res: Response) => {
     // * Grab needed elements from request body
     const { firstname, lastname, email, password } = req.body
 
     // * Check if user already exists
     const test = await User.findOne({ email })
     if (test) {
-        res.status(400).send("Email already exists!")
+        res.status(400).send('Email already exists!')
         return
     }
 
@@ -67,16 +74,17 @@ router.post("/signup", async (req: Request, res: Response) => {
         await user.save()
     } catch (err) {
         console.error(err.message)
-        res.status(401).send("Error during registration")
+        res.status(401).send('Error during registration')
         return
     }
 
     // * Login User
     req.login(user, err => {
         if (err) {
-            res.status(401).send("Error logging in")
+            res.status(401).send('Error logging in')
         } else {
-            res.status(200).send("OK")
+            /* Return user data is successfully signup */
+            res.status(200).json(stripData(user))
         }
     })
 })
