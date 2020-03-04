a0f61809abd17216e3ec2de8cfd0ef1926a3d950
anarmawala <arshadn00@gmail.com>
2020-03-04 16:34:04 -0600

    :ambulance: Fixed redux actions

--- client/src/models/redux/actions/auth.ts
+++ client/src/models/redux/actions/auth.ts
@@ -6,7 +6,7 @@ import axios from 'axios'
 export const getClasses = (): Action => {
     axios
         .get('/api/classes/userlist', { withCredentials: true })
-        .then(data => {
+        .then(({ data }) => {
             return {
                 type: 'SET_CLASSES',
                 payload: data
@@ -23,33 +23,59 @@ export const getClasses = (): Action => {
 export const logOut = (): Action => {
     axios.get('/api/auth/logout', { withCredentials: true })
 
+    window.location.href = '/'
     return {
         type: 'LOGOUT',
         payload: undefined
     }
 }
 
-export const logIn = (email: string, password: string): Action => {
+export const signUp = (
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string
+): Action => {
     axios
         .post(
-            '/api/auth/login',
-            {
-                email,
-                password
-            },
+            '/api/auth/signup',
+            { firstname, lastname, email, password },
             { withCredentials: true }
         )
-        .then(data => {
+        .then(({ data }) => {
+            window.location.href = '/classes/'
             return {
                 type: 'LOGIN',
                 payload: data
             }
         })
         .catch(err => {
+            window.location.href = '/'
             console.warn(err)
             return logOut()
         })
 
+    window.location.href = '/classes'
+    return logOut()
+}
+
+export const logIn = (email: string, password: string): Action => {
+    axios
+        .post('/api/auth/login', { email, password }, { withCredentials: true })
+        .then(({ data }) => {
+            window.location.href = '/classes/'
+            return {
+                type: 'LOGIN',
+                payload: data
+            }
+        })
+        .catch(err => {
+            window.location.href = '/'
+            console.warn(err)
+            return logOut()
+        })
+
+    window.location.href = '/classes'
     return logOut()
 }
 
@@ -57,13 +83,10 @@ export const addClass = (cls: Class): Action => {
     axios
         .post(
             '/api/classes/add',
-            {
-                subject: cls.title,
-                number: cls.number
-            },
+            { subject: cls.title, number: cls.number },
             { withCredentials: true }
         )
-        .then(data => {
+        .then(({ data }) => {
             return {
                 type: 'ADD_CLASS',
                 payload: data
@@ -87,7 +110,7 @@ export const removeClass = (cls: Class): Action => {
             },
             { withCredentials: true }
         )
-        .then(data => {
+        .then(({ data }) => {
             return {
                 type: 'REMOVE_CLASS',
                 payload: data
@@ -104,6 +127,7 @@ export const removeClass = (cls: Class): Action => {
 export const updateUser = (firstname: string, lastname: string): Action => {
     //TODO: Make request to update user
 
+    window.location.href = '/'
     return {
         type: 'UPDATE_USER',
         payload: null
