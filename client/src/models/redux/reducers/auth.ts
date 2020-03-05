d5447d3fa4e297164ec45d46b9b52614134b184f
anarmawala <arshadn00@gmail.com>
2020-03-05 11:08:34 -0600

    :sparkles Added Sign Up Functionality

--- client/src/models/redux/reducers/auth.ts
+++ client/src/models/redux/reducers/auth.ts
@@ -16,8 +16,12 @@ const initialState: AuthState = {
 
 export const Auth: Reducer<AuthState> = (state = initialState, action) => {
     switch (action.type) {
+        case 'SIGN_UP':
+            return { ...state, user: action.payload.user as User, error: action.payload.error }
         case 'SIGN_IN':
             return { ...state, user: action.payload.user as User, error: action.payload.error }
+        case 'SIGN_OUT':
+            return { ...state, user: null, error: action.payload }
         default:
             return state
     }
--- client/src/utils/functions/authentication.ts
+++ client/src/utils/functions/authentication.ts
@@ -1,6 +1,21 @@
 import axios from 'axios'
 import { store } from '@redux/store'
-import { userSignIn } from '@actions/auth'
+import { userSignIn, userSignOut, userSignUp } from '@actions/auth'
+
+export const signUp = async (fn: string, ln: string, em: string, pw: string) => {
+    try {
+        const response = await axios.post(
+            '/api/auth/signup',
+            { firstname: fn, lastname: ln, email: em, password: pw },
+            { withCredentials: true }
+        )
+
+        if (response.status == 200) store.dispatch(userSignUp(response.data, ''))
+        else store.dispatch(userSignIn(null, response.data))
+    } catch (err) {
+        store.dispatch(userSignIn(null, err))
+    }
+}
 
 export const signIn = async (email: string, password: string) => {
     try {
@@ -11,7 +26,20 @@ export const signIn = async (email: string, password: string) => {
         )
 
         if (response.status == 200) store.dispatch(userSignIn(response.data, ''))
+        else store.dispatch(userSignIn(null, response.data))
     } catch (err) {
         store.dispatch(userSignIn(null, err))
+    }
+}
+
+export const signOut = async () => {
+    try {
+        const response = await axios.get('/api/auth/logout', { withCredentials: true })
+
+        if (response.status == 200) store.dispatch(userSignOut(''))
+        else if (response.status == 401) store.dispatch(userSignOut('You are not signed in'))
+        else store.dispatch(userSignOut('Error Occured'))
+    } catch (err) {
+        store.dispatch(userSignOut(err))
     }
 }
