5a9bf45e36b900772ec65491a8851a669f0d87ce
Alex Chomiak <alex.chomiak@gmail.com>
2020-03-11 04:31:41 -0500

    :sparkles: migrated client to CRA client (for less multiplatform issues)

---
+++ cra-client/src/App.tsx
@@ -0,0 +1,30 @@
+/*
+ * File: /src/App.tsx
+ * File Created: Wednesday, 11th December 2019 11:29:00 pm
+ * Author: Alex Chomiak
+ *
+ * Last Modified: Sunday, 5th January 2020 4:29:11 pm
+ * Modified By: Alex Chomiak
+ *
+ * Author Github: https://github.com/alexchomiak
+ */
+
+import React, { FC } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Classes from './pages/Classes'
+import Account from './pages/Account'
+
+import NavBar from './components/NavBar'
+
+export const App: FC = () => (
+    <>
+        <Router>
+            <NavBar />
+            <Route exact path="/classes" component={Classes} />
+            <Route exact path="/account" component={Account} />
+            <Route exact path="/" component={Home} />
+        </Router>
+    </>
+)
---
+++ cra-client/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import { Provider } from 'react-redux'
+
+import 'react-app-polyfill/ie11'
+import 'bootstrap/dist/css/bootstrap.css'
+import * as serviceWorker from './serviceWorker'
+// ! Configure Redux Store
+import { store } from './models/redux/store'
+
+// * Import styles
+import './styles/index.scss'
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
