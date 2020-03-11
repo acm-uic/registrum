6a696e5a238ee87898ff75665e78199533a6183a
Alex Chomiak <alex.chomiak@gmail.com>
2020-03-11 04:31:41 -0500

    :sparkles: migrated client to CRA client (for less multiplatform issues)

---
+++ cra-client/src/components/SignUp.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+
+import { Button, Modal, Form } from 'react-bootstrap'
+
+import { signUp } from '../utils/functions/authentication'
+
+const SignUp = () => {
+    const [show, toggleShow] = useState(false)
+
+    const [fName, setFName] = useState('')
+    const [lName, setLName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+        <>
+            <span onClick={() => toggleShow(!show)}>Register</span>
+
+            <Modal show={show} onHide={() => toggleShow(!show)} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Sign Up</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control
+                                placeholder="Jon"
+                                value={fName}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    setFName(e.target.value)
+                                }}
+                            />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control
+                                placeholder="Doe"
+                                value={lName}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    setLName(e.target.value)
+                                }}
+                            />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    setEmail(e.target.value)
+                                }}
+                            />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    setPassword(e.target.value)
+                                }}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="primary"
+                        onClick={async (e: React.MouseEvent<HTMLButtonElement>) => {
+                            e.preventDefault()
+
+                            toggleShow(false)
+                            setFName('')
+                            setLName('')
+                            setEmail('')
+                            setPassword('')
+
+                            await signUp(fName, lName, email, password)
+                        }}
+                    >
+                        Sign Up
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default SignUp
---
+++ cra-client/src/models/redux/reducers/auth.ts
@@ -0,0 +1,36 @@
+import { Reducer } from './reducer'
+
+import { User } from '../../interfaces/User'
+
+export interface AuthState {
+    user: User | null
+    loading: boolean
+    error: string
+}
+
+const initialState: AuthState = {
+    user: null,
+    loading: false,
+    error: ''
+}
+
+export const Auth: Reducer<AuthState> = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SIGN_UP':
+            return { ...state, user: action.payload.user as User, error: action.payload.error }
+        case 'SIGN_IN':
+            return { ...state, user: action.payload.user as User, error: action.payload.error }
+        case 'SIGN_OUT':
+            return { ...state, user: null, error: action.payload }
+        case 'ADD_CLASS':
+            if (state.user !== null && action.payload.cls !== null)
+                return {
+                    ...state,
+                    user: { ...state.user, classes: [...state.user.classes, action.payload.cls] },
+                    error: action.payload.error
+                }
+            else return { ...state, error: action.payload.error }
+        default:
+            return state
+    }
+}
