7be9f12dba0cd375aeacea77676cf71e03943072
Arshad Narmawala <arshadn00@gmail.com>
2020-03-18 17:39:30 -0500

    :bug: Added Duplicate Email Response Code

--- client/src/utils/functions/authentication.ts
+++ client/src/utils/functions/authentication.ts
@@ -31,6 +31,15 @@ export const signUp = async (fn: string, ln: string, em: string, pw: string) => 
                     }
                 })
             )
+        else if (response.status === 400)
+            store.dispatch(
+                userSignUp(null, {
+                    msg: 'Email already exist! Please login',
+                    options: {
+                        type: 'info'
+                    }
+                })
+            )
         else
             store.dispatch(
                 userSignUp(null, {
