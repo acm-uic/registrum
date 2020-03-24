64ed01855edac167f92b310663edca7f0b6219d8
Alex Chomiak <alex.chomiak@gmail.com>
2020-03-23 21:16:54 -0500

    :bug: cannot provide redux state in jest rendered components?

--- client/config/settings.js
+++ client/config/settings.js
@@ -4,7 +4,7 @@
  * Author: Alex Chomiak
  *
  * Last Modified: Friday, 3rd January 2020 2:23:38 pm
- * Modified By: Alex Chomiak 
+ * Modified By: Alex Chomiak
  *
  * Author Github: https://github.com/alexchomiak
  */
@@ -21,5 +21,18 @@ module.exports = {
     includeReduxInBundle: false,
     // * Dev server settings
     port: 3001,
-    apiProxyUrl: process.env.API_PROXY_URL || 'http://localhost:4000'
+    apiProxyUrl: process.env.API_PROXY_URL || 'http://localhost:4000',
+    aliases: {
+        // ! Run npm run refresh to update the tsconfig when changing these!!
+        // ? Base directory is src for aliases
+        '@utils': ['utils'],
+        '@actions': ['models/redux/actions'],
+        '@reducers': ['models/redux/reducers'],
+        '@redux': ['models/redux'],
+        '@components': ['components'],
+        '@styles': ['styles'],
+        '@assets': ['assets'],
+        '@interfaces': ['models/interfaces'],
+        '@pages': ['pages']
+    }
 }
--- client/src/fileTransformer.js
+++ client/src/fileTransformer.js
@@ -3,7 +3,6 @@ const path = require('path')
 
 module.exports = {
     process(src, filename, config, options) {
-        console.log(filename)
         return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';'
     }
 }
