1123a6a8dc48125d7239740029a07fe181ac6e6c
Alex Chomiak <alex.chomiak@gmail.com>
2020-03-23 21:16:54 -0500

    :bug: cannot provide redux state in jest rendered components?

--- client/src/test/header.test.tsx
+++ client/src/test/header.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import NavBar from '../components/NavBar'
-
+import { Provider } from 'react-redux'
+import { store } from '../models/redux/store'
 it('renders', () => {
-    const result = shallow(<NavBar />).contains('Registrum')
+    const renderedComponent = shallow(
+        <Provider store={store}>
+            <NavBar />
+        </Provider>
+    ).dive()
+    console.log(renderedComponent.debug())
 
-    expect(result).toBeTruthy()
+    expect(renderedComponent.contains('Classes')).toBeTruthy()
 })
