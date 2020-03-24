6f36751c61ffbecfbaaa978797fcd72c1a29c558
Alex Chomiak <alex.chomiak@gmail.com>
2020-03-23 20:47:18 -0500

    :bug: trying to get jest to work

---
+++ client/src/test/test-setup.js
@@ -0,0 +1,8 @@
+global.requestAnimationFrame = function(callback) {
+    setTimeout(callback, 0)
+}
+
+const enzyme = require('enzyme')
+const Adapter = require('enzyme-adapter-react-16')
+
+enzyme.configure({ adapter: new Adapter() })
