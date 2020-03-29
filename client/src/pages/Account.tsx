bb4e3ea821520d3020385890c19de8a6f276b259
Jigar Patel <jigar.moyo@gmail.com>
2020-03-28 23:12:34 -0500

    added reactstrap components

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,7 +1,42 @@
 import React, { FC } from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 const Account: FC = () => {
-    return <div>Account</div>
+    return(
+        <div>
+            <h4>Account Information</h4>
+            <br/><br/>
+
+            <h5>Update Password</h5>
+
+            <Form>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter Password" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Re-type Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+
+            <br/><br/>
+
+            <h5>Update Name</h5>
+
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Account Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Name" />
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>
+
+        </div>
+    )
 }
 
 export default Account
