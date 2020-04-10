3d4864005814eb797f0914225fa7cdf3c5aeda94
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-04-10 12:52:37 -0500

    :sparkes: implemented banner-mongo sync utility

---
+++ banner-data/src/BannerData.ts
@@ -0,0 +1,236 @@
+import { Document, model, Schema, Mongoose } from 'mongoose'
+import Banner from './lib/Banner'
+
+interface Faculty extends Document {
+    bannerId: number;
+    category: string | null;
+    class: string;
+    courseReferenceNumber: number;
+    displayName: string;
+    emailAddress: string;
+    primaryIndicator: boolean;
+    term: number;
+}
+
+interface MeetingsFaculty extends Document {
+    category: string;
+    class: string;
+    courseReferenceNumber: string;
+    faculty: [];
+    meetingTime: {
+        beginTime: string;
+        building: string;
+        buildingDescription: string;
+        campus: string;
+        campusDescription: string;
+        category: string;
+        class: string;
+        courseReferenceNumber: string;
+        creditHourSession: number;
+        endDate: string;
+        endTime: string;
+        friday: boolean;
+        hoursWeek: number;
+        meetingScheduleType: string;
+        monday: boolean;
+        room: string;
+        saturday: boolean;
+        startDate: string;
+        sunday: boolean;
+        term: string;
+        thursday: boolean;
+        tuesday: boolean;
+        wednesday: boolean;
+    };
+    term: number;
+}
+
+interface Course extends Document {
+    id: number;
+    term: string;
+    termDesc: string;
+    courseReferenceNumber: string;
+    partOfTerm: string;
+    courseNumber: string;
+    subject: string;
+    subjectDescription: string;
+    sequenceNumber: string;
+    campusDescription: string;
+    scheduleTypeDescription: string;
+    courseTitle: string;
+    creditHours: string | null;
+    maximumEnrollment: number;
+    enrollment: number;
+    seatsAvailable: number;
+    waitCapacity: number;
+    waitCount: number;
+    waitAvailable: number;
+    crossList: string | null;
+    crossListCapacity: string | null;
+    crossListCount: string | null;
+    crossListAvailable: string | null;
+    creditHourHigh: string | null;
+    creditHourLow: number;
+    creditHourIndicator: string | null;
+    openSection: boolean;
+    linkIdentifier: string | null;
+    isSectionLinked: boolean;
+    subjectCourse: string;
+    faculty: Faculty[];
+    meetingsFaculty: MeetingsFaculty[];
+}
+
+const FacultySchema = new Schema({
+    bannerId: Number,
+    category: String,
+    class: String,
+    courseReferenceNumber: Number,
+    displayName: String,
+    emailAddress: String,
+    primaryIndicator: Boolean,
+    term: Number,
+})
+
+const MeetingsFacultySchema = new Schema({
+    category: String,
+    class: String,
+    courseReferenceNumber: String,
+    faculty: [FacultySchema],
+    meetingTime: {
+        beginTime: String,
+        building: String,
+        buildingDescription: String,
+        campus: String,
+        campusDescription: String,
+        category: String,
+        class: String,
+        courseReferenceNumber: String,
+        creditHourSession: Number,
+        endDate: String,
+        endTime: String,
+        friday: Boolean,
+        hoursWeek: Number,
+        meetingScheduleType: String,
+        monday: Boolean,
+        room: String,
+        saturday: Boolean,
+        startDate: String,
+        sunday: Boolean,
+        term: String,
+        thursday: Boolean,
+        tuesday: Boolean,
+        wednesday: Boolean,
+    },
+    term: Number,
+})
+
+const CourseSchema = new Schema({
+    id: Number,
+    term: String,
+    termDesc: String,
+    courseReferenceNumber: String,
+    partOfTerm: String,
+    courseNumber: String,
+    subject: String,
+    subjectDescription: String,
+    sequenceNumber: String,
+    campusDescription: String,
+    scheduleTypeDescription: String,
+    courseTitle: String,
+    creditHours: String,
+    maximumEnrollment: Number,
+    enrollment: Number,
+    seatsAvailable: Number,
+    waitCapacity: Number,
+    waitCount: Number,
+    waitAvailable: Number,
+    crossList: String,
+    crossListCapacity: String,
+    crossListCount: String,
+    crossListAvailable: String,
+    creditHourHigh: String,
+    creditHourLow: Number,
+    creditHourIndicator: String,
+    openSection: Boolean,
+    linkIdentifier: String,
+    isSectionLinked: Boolean,
+    subjectCourse: String,
+    faculty: [FacultySchema],
+    meetingsFaculty: [MeetingsFacultySchema],
+})
+
+type SearchResponse = {
+    success: boolean;
+    totalCount: number;
+    data: Course[];
+    pageOffset: number;
+    pageMaxSize: number;
+    sectionsFetchedCount: number;
+    pathMode: string;
+    searchResultsConfigs: {
+        config: string;
+        display: string;
+        title: string;
+        width: string;
+    }[];
+}
+
+export type BannerDataConfig = {
+    maxPageSize: number;
+    waitBetweenPages: number;
+    pageRetryCount: number;
+    pageRetryTime: number;
+}
+
+export class BannerData {
+    #db: Mongoose
+    #config: BannerDataConfig
+    constructor(db: Mongoose, config: BannerDataConfig) {
+        this.#db = db
+        this.#config = config
+    }
+
+    #getAllPages = async (banner: Banner) => {
+        const progress = '✨ 🚀 🌮 🧪 🎸 😎 🔫 💩 👽 👾 🤖 💥 🔥 🌈 👻'.split(' ')
+        const { maxPageSize, waitBetweenPages } = this.#config
+        const res = await this.#getPage(banner, maxPageSize, 0)
+        let received = res.data.length
+        let count = 0
+        while (received < res.totalCount) {
+            const page = await this.#getPage(banner, maxPageSize, received)
+            const { success, totalCount, pageOffset, pageMaxSize, sectionsFetchedCount } = page
+            console.log(`${progress[(count++) % progress.length]} ${success}, ${totalCount}, \
+            ${pageOffset}, ${pageMaxSize}, ${sectionsFetchedCount}\r`)
+            res.data = [...res.data, ...page.data]
+            received += page.data.length
+            await new Promise(resolve => setTimeout(resolve, waitBetweenPages))
+        }
+        return res
+    }
+
+    #getPage = async (banner: Banner, size: number, offset: number) => {
+        const { pageRetryTime, pageRetryCount } = this.#config
+        for (let retryCount = 0; retryCount < pageRetryCount; retryCount++) {
+            const res = await banner.search({ pageMaxSize: `${size}`, pageOffset: `${offset}` })
+            if (res.success)
+                return res
+            console.log('Retrying Page')
+            await new Promise(resolve => setTimeout(resolve, pageRetryTime))
+        }
+    }
+
+    updateDb = async () => {
+        const latestTerm = (await Banner.getTerm()).shift()
+        console.log(`🚨 Term: ${latestTerm.code}: ${latestTerm.description}`)
+        const banner = new Banner(latestTerm.code)
+        const res: SearchResponse = await this.#getAllPages(banner)
+        const CourseModel = model<Course>('Course', CourseSchema)
+        console.log('🗑 Deleting Documents')
+        await CourseModel.collection.deleteMany({})
+        console.log('✨ Creating Documents')
+        await CourseModel.collection.insertMany(res.data)
+        console.log('🍕 Sync Completed')
+    }
+}
+
+export default BannerData
---
+++ banner-data/src/lib/Banner.ts
@@ -0,0 +1,213 @@
+import axios, { AxiosInstance } from 'axios'
+import axiosCookieJarSupport from 'axios-cookiejar-support'
+import { CookieJar } from 'tough-cookie'
+import { URLSearchParams } from 'url'
+
+const bannerHost = process.env.BANNER_PROXY || 'https://banner.apps.uillinois.edu'
+
+type SearchProps = {
+    courseNumber?: string;
+    subject?: string;
+    startDate?: string;
+    endDate?: string;
+    pageOffset?: string;
+    pageMaxSize?: string;
+    sortColumn?: string;
+    sortDirection?: string;
+}
+
+type GetClassDetailsProps = {
+    courseReferenceNumber: string;
+    first?: string;
+}
+type GetCourseDescriptionProps = GetClassDetailsProps
+type GetSectionAttributesProps = GetClassDetailsProps
+type GetRestrictionsProps = GetClassDetailsProps
+type GetFacultyMeetingTimesProps = GetClassDetailsProps
+type GetXlstSectionsProps = GetClassDetailsProps
+type GetLinkedSectionsProps = GetClassDetailsProps
+type GetFeesProps = GetClassDetailsProps
+type GetSectionBookstoreDetailsProps = GetClassDetailsProps
+
+type GetTermProps = {
+    searchTerm?: string;
+}
+
+type GetSubjectProps = {
+    term: string;
+    searchTerm?: string;
+}
+
+type GetSessionProps = GetSubjectProps
+type GetAttributeProps = GetSubjectProps
+type GetPartOfTermProps = GetSubjectProps
+
+export class Banner {
+    #api: AxiosInstance
+    #cookieJar: CookieJar
+    #term: string
+    #subject: string
+
+    #clearCookie = () => (this.#cookieJar = new CookieJar())
+
+    #getNewCookie = async () => {
+        const params = new URLSearchParams({
+            mode: 'search',
+            term: this.#term,
+            studyPath: '',
+            studyPathText: '',
+            startDatepicker: '',
+            endDatepicker: '',
+            mepCode: '2UIC',
+        })
+        await this.#api.get(
+            `${bannerHost}/StudentRegistrationSSB/ssb/term/search?${params.toString()}`
+        )
+        await this.#api.get(`${bannerHost}/StudentRegistrationSSB/ssb/registration/registration`)
+    }
+
+    constructor(term: string, subject?: string) {
+        this.#term = term
+        this.#subject = subject || ''
+        this.#clearCookie()
+        axiosCookieJarSupport(axios)
+        this.#api = axios.create({ withCredentials: true, jar: this.#cookieJar })
+    }
+
+    search = async ({
+        courseNumber = '',
+        subject = '',
+        startDate = '',
+        endDate = '',
+        pageOffset = '0',
+        pageMaxSize = '10',
+        sortColumn = 'subjectDescription',
+        sortDirection = 'asc',
+    }: SearchProps) => {
+        if (this.#cookieJar.getCookiesSync(`${bannerHost}`)) {
+            this.#clearCookie()
+            await this.#getNewCookie()
+        }
+        const params = new URLSearchParams({
+            /* eslint-disable @typescript-eslint/camelcase */
+            txt_subject: subject || this.#subject,
+            txt_courseNumber: courseNumber,
+            txt_term: this.#term,
+            /* eslint-enable @typescript-eslint/camelcase */
+            startDatepicker: startDate,
+            endDatepicker: endDate,
+            pageOffset,
+            pageMaxSize,
+            sortColumn,
+            sortDirection,
+        })
+        return (
+            await this.#api.get(
+                `${bannerHost}/StudentRegistrationSSB/ssb/searchResults/\
+                searchResults?${params.toString()}`
+            )
+        ).data
+    }
+
+    #courseOperation = async (
+        operation: string,
+        params:
+            | GetClassDetailsProps
+            | GetCourseDescriptionProps
+            | GetSectionAttributesProps
+            | GetRestrictionsProps
+            | GetFacultyMeetingTimesProps
+            | GetXlstSectionsProps
+            | GetLinkedSectionsProps
+            | GetFeesProps
+            | GetSectionBookstoreDetailsProps
+    ) => {
+        if (this.#cookieJar.getCookiesSync(`${bannerHost}`)) {
+            this.#clearCookie()
+            await this.#getNewCookie()
+        }
+        const _params = new URLSearchParams({
+            ...params,
+            term: this.#term,
+            first: 'first',
+        })
+        return (
+            await this.#api.get(
+                `${bannerHost}/StudentRegistrationSSB/ssb/searchResults/${operation}?${_params.toString()}`
+            )
+        ).data
+    }
+
+    getClassDetails = async (params: GetClassDetailsProps) => {
+        return await this.#courseOperation('getClassDetails', params)
+    }
+    getCourseDescription = async (params: GetCourseDescriptionProps) => {
+        return await this.#courseOperation('getCourseDescription', params)
+    }
+    getSectionAttributes = async (params: GetSectionAttributesProps) => {
+        return await this.#courseOperation('getSectionAttributes', params)
+    }
+    getRestrictions = async (params: GetRestrictionsProps) => {
+        return await this.#courseOperation('getRestrictions', params)
+    }
+    getFacultyMeetingTimes = async (params: GetFacultyMeetingTimesProps) => {
+        return await this.#courseOperation('getFacultyMeetingTimes', params)
+    }
+    getXlstSections = async (params: GetXlstSectionsProps) => {
+        return await this.#courseOperation('getXlstSections', params)
+    }
+    getLinkedSections = async (params: GetLinkedSectionsProps) => {
+        return await this.#courseOperation('getLinkedSections', params)
+    }
+    getFees = async (params: GetFeesProps) => {
+        return await this.#courseOperation('getFees', params)
+    }
+    getSectionBookstoreDetails = async (params: GetSectionBookstoreDetailsProps) => {
+        return await this.#courseOperation('getSectionBookstoreDetails', params)
+    }
+
+    private static staticOperations = async (
+        operation: string,
+        params?:
+            | GetTermProps
+            | GetSubjectProps
+            | GetSessionProps
+            | GetPartOfTermProps
+            | GetAttributeProps
+    ) => {
+        const _params = new URLSearchParams({
+            ...params,
+            searchTerm: '',
+            offset: '1',
+            max: '1000',
+            mepCode: '2UIC',
+        })
+        return (
+            await axios.get(
+                `${bannerHost}/StudentRegistrationSSB/ssb/classSearch/${operation}?${_params.toString()}`
+            )
+        ).data
+    }
+
+    public static getTerm = async (params?: GetTermProps) => {
+        return await Banner.staticOperations('getTerms', params)
+    }
+
+    public static getSubject = async (params: GetSubjectProps) => {
+        return await Banner.staticOperations('get_subject', params)
+    }
+
+    public static getSession = async (params: GetSessionProps) => {
+        return await Banner.staticOperations('get_session', params)
+    }
+
+    public static getPartOfTerm = async (params: GetPartOfTermProps) => {
+        return await Banner.staticOperations('get_partOfTerm', params)
+    }
+
+    public static getAttribute = async (params: GetAttributeProps) => {
+        return await Banner.staticOperations('get_attribute', params)
+    }
+}
+
+export default Banner
