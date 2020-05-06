5485a9655f2cda84c56507cb8c0d93abcd8ee223
Arshad Narmawala <arshadn00@gmail.com>
2020-05-06 02:09:43 -0500

    :bug: Fix Testing

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,14 +1,7 @@
 module.exports = {
     collectCoverage: true,
     coverageDirectory: './coverage/',
-    globals: {
-        'ts-jest': {
-            packageJson: 'package.json'
-        }
-    },
     coverageDirectory: './coverage/',
-    preset: 'ts-jest',
-    testEnvironment: 'node',
     setupFiles: ['<rootDir>/src/test/test-setup.js'],
     testMatch: ['<rootDir>/src/test/*.test.(ts|tsx|js)'],
     modulePaths: ['<rootDir>'],
@@ -17,7 +10,7 @@ module.exports = {
         '\\.(css|less)$': 'identity-obj-proxy'
     },
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.tsx?$': 'babel-jest',
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/src/fileTransformer.js'
     }
