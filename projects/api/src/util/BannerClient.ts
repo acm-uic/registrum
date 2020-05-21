1c83f5a1c1da2e2a5f69630a919b4e299d79a7ef
Arshad Narmawala <arshadn00@gmail.com>
2020-05-21 05:50:07 -0500

    :fire: Remove interceptors

--- projects/api/src/util/BannerClient.ts
+++ projects/api/src/util/BannerClient.ts
@@ -1,23 +1,13 @@
-import axios, { AxiosRequestConfig, AxiosResponse, AxiosInstance } from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
 // * Create Banner Data
 export class BannerClient {
     client: AxiosInstance
     bannerUrl: string
     constructor(bannerUrl: string) {
-        console.log(`CREATING BANNER CLIENT WITH URI ${bannerUrl}`)
         this.client = axios.create({
             baseURL: bannerUrl,
             withCredentials: true
-        })
-        this.client.interceptors.request.use((req: AxiosRequestConfig) => {
-            console.log(`BANNER REQ >> ${req.url}`)
-            return req
-        })
-
-        this.client.interceptors.response.use((res: AxiosResponse) => {
-            console.log(`BANNER RESPONSE << ${JSON.stringify(res.data)}`)
-            return res
         })
     }
 }
