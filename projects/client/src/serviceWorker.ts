28b6c83c00ec3c292363e9ef8a7fde3bc4ba0191
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-05-22 16:35:06 -0500

    pwa install patch

--- projects/client/src/serviceWorker.ts
+++ projects/client/src/serviceWorker.ts
@@ -95,3 +95,18 @@ self.addEventListener('push', event => {
     // * Show the notification
     event.waitUntil(self.registration.showNotification(title, options))
 })
+
+self.addEventListener('install', event => {
+    console.log('👷', 'install', event)
+    self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+    console.log('👷', 'activate', event)
+    return self.clients.claim()
+})
+
+self.addEventListener('fetch', function (event) {
+    // console.log('👷', 'fetch', event);
+    event.respondWith(fetch(event.request))
+})
